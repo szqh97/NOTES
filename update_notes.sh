735b1397a8bf829f6fd2275e09d5eeac12647856
szqh97 <szqh97@163.com>
2014-10-29 13:47:55 +0800

    update update_notes.sh

--- update_notes.sh
+++ update_notes.sh
@@ -3,6 +3,7 @@ ssh_auth_sock=`find /tmp -iname "keyring*"`
 export SSH_AUTH_SOCK=${ssh_auth_sock}/ssh
 git status |grep "cherrytree.ctd"
 if [ $? != "1" ]; then
+    git pull
     git add cherrytree.ctd;
     git commit -m "update notes"; 
     git push
